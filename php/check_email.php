522a88bae381aaef6b6abfd56bacfe3a455173af
Shreya Sharma <s.shreyaa84429@gmail.com>
2019-12-16 12:22:26 +0530

    fixed all checks on user registration

--- php/check_email.php
+++ php/check_email.php
@@ -2,11 +2,12 @@
 include('conn.php');
 
 $emailinput = $_POST[email];
+$tablename = $_POST[tablename];
 if (isset($_POST[id])) {
     $id = $_POST[id];
-    $sql = "select * from student where email= '$emailinput' and id!=$id;";
+    $sql = "select * from $tablename where email= '$emailinput' and id!=$id;";
 } else
-    $sql = "select * from student where email= '$emailinput';";
+    $sql = "select * from $tablename where email= '$emailinput';";
 $result = $conn->query($sql);
 $n = $result->num_rows;
 if ($n > 0) {
