bff93bfc5f0b82307841bb07d1e9f67f4c54f05c
Shreya Sharma <s.shreyaa84429@gmail.com>
2019-12-17 21:58:46 +0530

    finalized checks on user and canteen registration

--- php/check.php
+++ php/check.php
@@ -4,11 +4,7 @@ include('conn.php');
 $input =$_POST[input];
 $inputvalue = $_POST[inputvalue];
 $tablename= $_POST[tablename];
-if (isset($_POST[id])) {
-    $id = $_POST[id];
-    $sql = "select * from $tablename where $input= '$inputvalue' and id!=$id;";
-} else {
-    $sql = "select * from $tablename where $input= '$inputvalue';"; }
+$sql = "select * from $tablename where $input= '$inputvalue';";
 $result = $conn->query($sql);
 $n = $result->num_rows;
 if ($n > 0) {
