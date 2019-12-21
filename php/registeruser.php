9c5c7f6cb4a6a982d6694defdb582a3b96384bbe
Shreya Sharma <s.shreyaa84429@gmail.com>
2019-12-21 22:13:20 +0530

    fixed login

--- php/registeruser.php
+++ php/registeruser.php
@@ -16,13 +16,9 @@ $sql="insert into student(name,enroll,username,password,mobile,bhawan,room,email
 
  if ($conn->query($sql)) {
        echo true;
-      $host  = $_SERVER['HTTP_HOST'];
-      $uri="/html/login.html";
-      $index_url="http://".$host.$uri;
-header( "Location: $index_url" );
 }
 else {
-  echo "Registration failed";
+  echo false ;
 }
 $conn->close();
  ?>
